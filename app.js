e06b752be75726d58625b0efe7480932e40811c6
valentinewabwire <valentine.w.maita@gmail.com>
2022-10-31 16:39:20 +0300

    Passport to add Cookies and Sessions

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 /* Creating an instance of the express module. */
 const app = express();
@@ -18,7 +18,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 /* Telling the server to use the public folder as a static folder. */
 app.use(express.static("public"));
-
+/* This is a middleware that is being used to create a session for the user. */
+app.use(
+  session({
+    secret: "Our little Secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+/* This is a middleware that is being used to create a session for the user. */
+app.use(passport.initialize());
+app.use(passport.session());
 /* Connecting to the database. */
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -28,8 +38,15 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+/* A method that is being used to hash and salt the password. */
+userSchema.plugin(passportLocalMongoose);
+
 /* Creating a new model called User. */
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 /* Telling the server to render the home.ejs file when the user visits the root route. */
 app.get("/", function (req, res) {
@@ -46,37 +63,58 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
-/* Creating a new user and saving it to the database. */
-app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
 });
 
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({ email: username }, function (err, foundUser) {
+/* This is a route that is being created. When the user clicks logout route, the server will
+logout the user and redirect the user to the root route. */
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
+      res.redirect("/");
+    }
+  });
+});
+
+/* Creating a new user and saving it to the database. */
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
         });
       }
+    }
+  );
+});
+
+/* This is a route that is being created. When the user visits the /login route, the server will render
+the login.ejs file. */
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     }
   });
 });
